b512e021b31cf34dd1bb1c4f2a17024e5dfb459e
danish <1211103424@student.mmu.edu.my>
2024-10-26 07:43:56 +0800

    update

--- portfolio-website/src/components/Intro.js
+++ portfolio-website/src/components/Intro.js
@@ -1,19 +1,23 @@
 import { Container, Col, Row, Button } from 'react-bootstrap';
-import luffy from '../assets/luffy.jpg';
+import icon from '../assets/icon.png';
 
 const Intro = () => {
     return (
         <div className='intro'>
             <Container className='text-black text-center d-flex justify-content-center align-items-center'>
                 <Row>
-                    <Col><div className="image m-4">
-    <img src={luffy} alt="Luffy" />
-  </div></Col>
-                    <Col><div className='motto mt-5'>Vision, Moving Forward</div>
-                        <div className='description mt-4'>I'm Afiq Danish, a software engineer passionate about creating efficient and user-friendly solutions. Currently pursuing my final year, refining my skills in web development and AI.</div>
+                    <Col>
+                    <div className="image m-7 ">
+                        <img src={icon} alt="icon" />
+                    </div>
+                    </Col>
+                    <Col><div className='motto mt-5'>Hello everyone, I'm <br/>Afiq Danish</div>
+                        <div className='description mt-2'>  Computer Science Student & <span style={{ fontWeight: 'bold' }}>Tech Enthusiast</span>
+                        </div>
+                        <div className='quotes mt-4'>per aspera, ad astra</div>
                         <div className='introButton mt-4'>
-                            <Button variant="dark" className='m-3'>My Project</Button>
-                            <Button variant="dark">Contact Me</Button>
+                            <Button className='m-3'>My Project</Button>
+                            <Button className='m-3'>Contact Me</Button>
                         </div>
 
                     </Col>
